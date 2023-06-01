cac1d1aff857e76124cb0018e90fd748aba83b58
alexiscyber14 <142sledgehammer14@gmail.com>
2023-06-01 07:51:27 +0100

    Implement the reverse method in solver:tests pass

--- solver.rb
+++ solver.rb
@@ -1,8 +1,11 @@
 class Solver
   def factorial(num)
     raise ArgumentError, 'Factorial is not defined for negative numbers.' if num < 0
-
     (1..num).reduce(1, :*)
-
   end
+
+  def reverse(word)
+    word.reverse
+  end
+
 end
