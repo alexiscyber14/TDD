f372f76de217283f6ebd6ee47d0400bd9a31ded8
alexiscyber14 <142sledgehammer14@gmail.com>
2023-06-01 07:46:29 +0100

    Complete factoril method:tests pass

--- solver.rb
+++ solver.rb
@@ -1,9 +1,8 @@
 class Solver
   def factorial(num)
-    if num == 0
-      1
-    else
-      # Placeholder for now
-    end
+    raise ArgumentError, 'Factorial is not defined for negative numbers.' if num < 0
+
+    (1..num).reduce(1, :*)
+
   end
 end
