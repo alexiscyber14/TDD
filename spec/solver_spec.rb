04d0217a810e91c57a191f8856023e6a310e6dff
alexiscyber14 <142sledgehammer14@gmail.com>
2023-06-01 10:09:14 +0100

    fix test

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -4,17 +4,24 @@ RSpec.describe Solver do
   let(:solver) { Solver.new }
 
   describe '#factorial' do
-    it 'returns the factorial of a number' do
+    it 'returns the factorial of 0' do
       expect(solver.factorial(0)).to eq(1)
     end
+
     it 'returns the factorial of 1' do
       expect(solver.factorial(1)).to eq(1)
     end
+
     it 'returns the factorial of a positive number' do
       expect(solver.factorial(5)).to eq(120)
       expect(solver.factorial(10)).to eq(3_628_800)
     end
+
+    it 'raises an exception for a negative number' do
+      expect { solver.factorial(-1) }.to raise_error(ArgumentError)
+    end
   end
+
 
   describe '#reverse' do
     it 'returns the reversed word' do
